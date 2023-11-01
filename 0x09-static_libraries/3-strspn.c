f41ab4047afa7861a277a2007123dc0fb8b6bf57
Nomcebo1992 <snembambo@gmail.com>
2023-11-01 07:28:02 -0700

    3-strspn.c

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
+
+
+unsigned int _strspn(char *s, char *accept)
+
+{
+
+    return strspn(s, accept);
+
+nclude "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
+
+
+unsigned int _strspn(char *s, char *accept)
+
+{
+
+    return strspn(s, accept);
+
+}}
